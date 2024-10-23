8f4ff56e205d3f425e11fc07d445e261ff99e4f1
K-sama <drumer19861018@gmail.com>
2024-10-23 23:36:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php include 'layout/header.php'; ?>
+
+
+<div>
+        You are not logged in <a href='login.php'>Login now</a>
+    </div> 
+    <!-- <div>
+        You are logged in <a href='logout.html'>Logout</a>
+    </div>         -->
+
+
+<?php include 'layout/footer.php'; ?>
---
+++ layout/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <h3>Footer section </h3>
+</footer>
+
+<script src="build/js/main.js" defer></script>
+</body>
+</html>
